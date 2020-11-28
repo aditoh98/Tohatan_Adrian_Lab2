4ca50bb9bf5482ad8a4ce45563327d1afc5c369a
ADRIAN-SERGIU TOHĂTAN <adrian.tohatan@stud.ubbcluj.ro>
2020-11-28 12:13:33 +0200

    lab8

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,9 @@ namespace Tohatan_Adrian_Lab2.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Chat()
+        {
+            return View();
+        }
     }
 }
