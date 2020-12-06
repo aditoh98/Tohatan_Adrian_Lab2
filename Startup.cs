cba25f00cf20470049561a228fa31d147b31e12d
ADRIAN-SERGIU TOHĂTAN <adrian.tohatan@stud.ubbcluj.ro>
2020-12-06 13:32:53 +0200

    lab9

--- Startup.cs
+++ Startup.cs
@@ -50,6 +50,7 @@ namespace Tohatan_Adrian_Lab2
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -58,6 +59,7 @@ namespace Tohatan_Adrian_Lab2
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapHub<ChatHub>("/chathub");
+                endpoints.MapRazorPages();
             });
         }
     }
